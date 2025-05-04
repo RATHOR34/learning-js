888d0d948f94fc851276a63ca1bd4279b996e427
RATHOR34 <rathorchanchal695@gmail.com>
2025-05-04 21:45:45 +1000

    js operator practice

---
+++ javascript practice/ArithmaticOperator.js
@@ -0,0 +1,57 @@
+let a = 14;
+let b = 5;
+b=56
+let c = a+b;
+console.log(c);      // output will get 9
+c++                // postfix increment operator
+console.log(++c)      // prefix increment operator
+ 
+{
+    let c= a-b
+    console.log(c)
+}
+
+var x = 10;
+var y = 30;
+var x = 30
+const m = 10;
+x--  // postfix decrement operator
+console.log(--x)   // prefix decrement operator
+
+//(=) simple assignment operator
+let email = "chanchal@gmail.com";
+
+let num = 10;
+num += 5   // Addition Assignment Operator 
+num -= 5   // Subtraction Assignment Operator 
+num *= 5   // Multiplication Assignment Operator 
+num /= 5   // Division Assignment Operator 
+num %= 5   // Remainder Assignment Operator 
+5 != 5     //not equal operator;
+5 !== 5     //strict inequality operator
+5 === 5      //strict equality operator 
+5 == 5       //equality operator
+
+// && and operator          
+// || OR operator      
+
+// ternary operator in js
+//  let message = condition ? value for true : value for false;
+let age = 18    
+let message = (age>18) ?"Yes" : "No"
+console.log(message)
+
+console.log(5 != 5) //false
+
+console.log(5 == 5)  //true
+
+console.log(5 === "5")  //false
+
+console.log(5 == "5")  //true
+
+let greet ="hello"
+
+greet+=" world"
+
+console.log(greet)
+
